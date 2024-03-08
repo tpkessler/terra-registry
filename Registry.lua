60c505a0ab8d21bb535822b5a6d40f2546ed0c87
Torsten Keßler <t.kessler@posteo.de>
2024-03-08 11:40:28 +0100

    Initialized new registry.

---
+++ Registry.lua
@@ -0,0 +1,9 @@
+Registry = {
+    name = "public",
+    uuid = "44df1ab8-00b0-421f-8bcc-15ba1e7842dd",
+    url  = "git@github.com:tpkessler/terra-registry.git",
+    description = "Cosm local package registry",
+    packages = {
+    },
+}
+return Registry
